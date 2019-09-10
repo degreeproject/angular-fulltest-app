1582ca3452acad55edc0ce23f1adc083b1308888
Niclas Olsson <nicolss@kth.se>
2019-09-10 14:05:38 +0200

    moved server to a new folder

--- server/server.js
+++ server/server.js
@@ -4,13 +4,15 @@ const path = require('path');
 
 const app = express();
 
+const port = process.env.PORT || 8080;
+
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/angular-performance-test'));
+app.use(express.static(__dirname + '/../dist/angular-performance-test'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/angular-performance-test/index.html'));
+res.sendFile(path.join(__dirname+'/../dist/angular-performance-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => console.log(`Server running on port ${port}`));
