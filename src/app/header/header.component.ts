179ea66490100c006c4c77e3597e05dccf16b511
Niclas Olsson <nicolss@kth.se>
2019-10-02 11:49:33 +0200

    state managemanet

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './../store/app.states';
+import { Observable } from 'rxjs';
+import { userState } from './../models/userState.model';
+import { Router } from '@angular/router';
+import * as authActions from './../store/actions/auth.actions';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userState: Observable<userState>;
 
-  loggedIn = false;
+  stateUser: userState;
 
+  constructor(private store: Store<AppState>, private router: Router) {
+    store.select('userState').subscribe(data => {
+      this.stateUser = data;
+    });
+  }
+
+  isLoggedIn() {
+    if (typeof(this.stateUser[0]) === 'undefined') {
+      return false;
+    } else {
+      return this.stateUser[0].loggedIn;
+    }
+  }
+
+  checkState() {
+    console.log(this.stateUser);
+  }
+  logout() {
+    this.removeUserState();
+    this.router.navigate(['/login']);
+  }
+  removeUserState() {
+    this.store.dispatch(new authActions.RemoveState(0));
+  }
   ngOnInit() {
+
   }
 
 }
